9029bd096fb5bbc01df8111c6c8ef06b107db168
Akshathaap <89653099+Akshathaap@users.noreply.github.com>
2023-04-23 03:12:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let socials_button=document.getElementById("socials_button");
+let socials=document.getElementById("socials");
+socials_button.addEventListener("click", () => {
+    socials.style.display="block";
+    console.log("Hi");
+});
